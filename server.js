f62114e303641380d3d6daa76783ba9d7b2bea8c
filippoipp <filippoipp@hotmail.com>
2020-04-28 14:33:50 -0300

    start on package json

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.use('/', serverStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8080
 app.listen(port);
 
-console.log('Listening on port:' + port);
+console.log('Listening on port:' + port)
